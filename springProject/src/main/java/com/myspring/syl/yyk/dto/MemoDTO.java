8a99f8115fbca8be4fa4db9da8a58408776a47a9
yeokyeong <durud2604@naver.com>
2022-10-24 17:17:23 +0900

    memoDTO

---
+++ springProject/src/main/java/com/myspring/syl/yyk/dto/MemoDTO.java
@@ -0,0 +1,27 @@
+package com.myspring.syl.yyk.dto;
+
+public class MemoDTO {
+
+	private String memoNum;
+	private String m_content;
+	private String id;
+	
+	public String getMemoNum() {
+		return memoNum;
+	}
+	public void setMemoNum(String memoNum) {
+		this.memoNum = memoNum;
+	}
+	public String getM_content() {
+		return m_content;
+	}
+	public void setM_content(String m_content) {
+		this.m_content = m_content;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
